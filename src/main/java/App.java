bff89259ec5141b968550f3841421c5ab6173d5e
Fedichkin <fedor_fedichkin@epam.com>
2017-12-05 15:32:35 +0300

    Simple project was created

---
+++ src/main/java/App.java
@@ -0,0 +1,23 @@
+public class App implements EventLogger{
+    private Client client;
+    private ConsoleEventLogger consoleEventLogger;
+    private EventLogger eventLogger;
+
+    public App() {
+    }
+
+    public App(Client client, EventLogger eventLogger) {
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
+
+    public static void main(String[] args) {
+
+//        app.logEvent("Some event for user 1");
+    }
+
+    public void logEvent(String msg) {
+        String message = msg.replaceAll(client.getId(), client.getFullName());
+        eventLogger.logEvent(message);
+    }
+}
